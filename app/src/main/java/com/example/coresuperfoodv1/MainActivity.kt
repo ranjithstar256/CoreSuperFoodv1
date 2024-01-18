d4a1d49c0ba5207ab29cbc533d7d023c7723ca1a
RiteshSingh8759 <ritesh8759@gmail.com>
2024-01-18 17:53:39 +0530

    ritesh code thursday eve

--- app/src/main/java/com/example/coresuperfoodv1/MainActivity.kt
+++ app/src/main/java/com/example/coresuperfoodv1/MainActivity.kt
@@ -1,35 +1,57 @@
 package com.example.coresuperfoodv1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.coresuperfoodv1.ui.theme.CoreSuperFoodv1Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +63,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CenterAlignedTopAppBarExample()
+                    Column {
+                        Greeting()
+                        Spacer(modifier = Modifier.padding(0.5.dp))
+                        Top()
+                    }
                 }
             }
         }
@@ -51,42 +77,83 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    TopAppBar (
-        title = {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = "Test"
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.End
-                ) {
-                    Icon(
-                        painter = painterResource(id = android.R.drawable.star_big_on),
-                        contentDescription = "Image")
-                }
-            }
-        }
-    )
+fun Greeting() {
+    Row(
+        Modifier
+            .background(Color(0xFF6f1dc9))
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "NO DAIRY, NO PRESERVATIVES, NO ADDED SUGAR",
+        color = Color.White, fontWeight = FontWeight.Bold,
+        modifier = Modifier.weight(1f))
+        Text(text = "FOR SUBSCRIPTION WHATSAPP US ON 8826683886",
+        color = Color.White, fontWeight = FontWeight.Bold,
+        modifier = Modifier.weight(1f))
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CoreSuperFoodv1Theme {
-        Greeting("Android")
-        CenterAlignedTopAppBarExample()
+        Greeting()
+        Top()
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun Top()
+{
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
+    val scope =  rememberCoroutineScope()
+
+    Scaffold(
+        scaffoldState=scaffoldState,
+        topBar = { CenterAlignedTopAppBarExample(
+            navController =navController,
+            onNavigationIconClick = {
+                scope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            }
+        ) }, drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+
+            DrawerHeader()
+            var  items = listOf(
+                MenuItem(
+                    id = "Shop",
+                title = "Shop",
+            contentDescription = "Go to Shop"
+
+            ),
+            MenuItem(
+                id = "Our Story",
+            title = "Our Story",
+            contentDescription = ""
+
+            ),
+            MenuItem(
+                id = "Customer speak",
+            title = "Customer speak",
+            contentDescription = "Customer speak"
+
+            )
+            )
+            DrawerBody(items)
+        }
+
+    ) {
+        it
+    }
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CenterAlignedTopAppBarExample() {
+fun CenterAlignedTopAppBarExample(navController: NavController,
+                                  onNavigationIconClick: () -> Unit,) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
@@ -99,16 +166,14 @@ fun CenterAlignedTopAppBarExample() {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 ),
                 title = {
-                    Text(
-                        "Centered Top App Bar",
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis
-                    )
+                        Image(painterResource(id = R.drawable.actionbarimage), contentDescription = null,
+
+                            modifier = Modifier.padding(15.dp))
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = onNavigationIconClick) {
                         Icon(
-                            imageVector = Icons.Filled.ArrowBack,
+                            imageVector = Icons.Filled.Menu,
                             contentDescription = "Localized description"
                         )
                     }
@@ -116,11 +181,18 @@ fun CenterAlignedTopAppBarExample() {
                 actions = {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
-                            imageVector = Icons.Filled.Menu,
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ShoppingCart,
                             contentDescription = "Localized description"
                         )
                     }
                 },
+
                 scrollBehavior = scrollBehavior,
             )
         },
@@ -133,4 +205,47 @@ fun CenterAlignedTopAppBarExample() {
 @Composable
 fun ScrollContent(innerPadding: PaddingValues) {
    // TODO("Not yet implemented")
+}
+
+@Composable
+fun DrawerHeader(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 64.dp)
+        ,
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Text(text = "Header", fontSize = 60.sp)
+    }
+}
+@Composable
+private fun DrawerMenuItem(
+    iconDrawableId: Int,
+    text: String,
+    onItemClick: () -> Unit){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick() }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Icon(
+            painter = painterResource(iconDrawableId),
+            contentDescription = null,
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = text, )
+    }
+}
+@Composable
+fun DrawerBody(items: List<MenuItem>) {
+
+    items.forEach { menuItem ->
+
+            Text(text = menuItem.title, fontSize = 20.sp,modifier=Modifier.padding(15.dp))
+
+
+    }
 }
---
+++ app/src/main/java/com/example/coresuperfoodv1/MenuItem.kt
@@ -0,0 +1,9 @@
+package com.example.coresuperfoodv1
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class MenuItem(
+    val id: String,
+    val title: String,
+    val contentDescription: String
+)
